06408f87522151e9d9484e593ada901165078043
doncharacterx <80598954+doncharacterx@users.noreply.github.com>
2021-09-08 13:17:03 -0500

    Add files via upload

---
+++ todo-app.js
@@ -0,0 +1,84 @@
+const INITAL_STATE = {
+    
+    todos: []
+  }
+  function reducer(state = INITAL_STATE, action) {
+    switch(action.type) {
+      case "ADD_TODO": {
+        const newTodo = {
+          id: action.id,
+          title: action.title,
+          completed: false,
+        }
+        return {
+          ...state,
+          todos: [...state.todos, newTodo]
+        }
+      }
+      case "REMOVE_TASK": {
+        const newTodos = state.todos.filter(todo =>
+          todo.id !== action.id
+        );
+        return {...state, todos: newTodos }
+      }
+      case "TOGGLE_COMPLETED": {
+        const newTodos = state.todos.map(todo =>
+          todo.id === action.id ? { ...todo, completed: !todo.        completed } : todo
+        );
+        return { ...state, todos: newTodos };
+      }
+      case "CLEAR_COMPLETED": {
+       const filteredTodos = state.todos.filter(todo => !todo.     completed)
+        return { ...state, todos: filteredTodos }
+      }
+      default: return state
+    }
+ }
+ const store = window.Redux.createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+ store.subscribe(render);
+
+ const clearTodosButton = document.getElementById("clear-todos");
+clearTodosButton.addEventListener("click", () =>
+  store.dispatch({ type: "CLEAR_COMPLETED" })
+);
+
+ const form = document.getElementById("add-todo");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const inputValue = event.target.elements.todoText.value;
+  store.dispatch({
+    type: "ADD_TODO",
+    title: inputValue,
+    id: Date.now()
+  });
+  event.target.elements.todoText.value = "";
+});
+
+
+ const todosContainer = document.getElementById("todos-container");
+function render() {
+    todosContainer.innerHTML = "";
+    const { todos } = store.getState();
+    todos.forEach(todo => {
+      const task = document.createElement("li");
+      task.textContent = todo.title;
+      if (todo.completed) {
+        task.style.textDecoration = "line-through";
+      }
+      createButton(task, "Remove", "REMOVE_TASK", todo.id);
+      createButton(task, "Toggle done", "TOGGLE_COMPLETED", todo.    id);
+      todosContainer.appendChild(task);
+    });
+  }
+  function createButton(parent, text, type, id) {
+    const btn = document.createElement("button");
+    btn.textContent = text;
+    btn.addEventListener("click", () =>
+      store.dispatch({ type, id })
+    );
+    parent.appendChild(btn);
+  }
+ 
